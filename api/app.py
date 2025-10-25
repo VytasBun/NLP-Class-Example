6daf174d9650d187a67ae7258bbe3738bd8c82b8
VytasBun <129725021+VytasBun@users.noreply.github.com>
2025-10-24 23:10:42 -0500

    Last one =)

--- api/app.py
+++ api/app.py
@@ -9,6 +9,7 @@ from nltk import word_tokenize, pos_tag
 nltk.download('punkt')
 nltk.download('punkt_tab')
 nltk.download('averaged_perceptron_tagger')
+nltk.download('averaged_perceptron_tagger_eng')
 
 app = Flask(__name__)
 
